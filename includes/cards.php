b5e627b3717dd3f397c81bea7d26007d08c444c0
Leonardo de Souza <contato@leonardosouza.com.br>
2024-03-22 21:05:34 -0300

    ajustes

--- includes/cards.php
+++ includes/cards.php
@@ -1,71 +1,71 @@
 <div class="hvr-grow">
-    <img src="../assets/images/covers/objetivos-dia.png">
+    <a href="comunidade.php"><img src="../assets/images/covers/objetivos-dia.png"></a>
     <span>Comunidade</span>
     <div class="mask"></div>
 </div>
 
 <div class="hvr-grow">
-    <img src="../assets/images/covers/desafio-modo-caverna.png">
+    <a href="desafio.php"><img src="../assets/images/covers/desafio-modo-caverna.png"></a>
     <span><small>Desafio</small> Modo Caverna</span>
     <div class="mask"></div>
 </div>
 
 <div class="hvr-grow">
-    <img src="../assets/images/covers/metas.png">
+    <a href="metas.php"><img src="../assets/images/covers/metas.png"></a>
     <span>Metas</span>
     <div class="mask"></div>
 </div>
 
 <div class="hvr-grow">
-    <img src="../assets/images/covers/agenda.png">
+    <a href="compromissos.php"><img src="../assets/images/covers/agenda.png"></a>
     <span>Compromissos e Objetivos</span>
     <div class="mask"></div>
 </div>
 
 <div class="hvr-grow">
-    <img src="../assets/images/covers/rituais.png">
+    <a href="rituais.php"><img src="../assets/images/covers/rituais.png"></a>
     <span>Rituais</span>
     <div class="mask"></div>
 </div>
 
 <div class="hvr-grow">
-    <img src="../assets/images/covers/treino-dieta.png">
+    <a href="treino-alimentacao.php"><img src="../assets/images/covers/treino-dieta.png"></a>
     <span>Treino e Alimentação</span>
     <div class="mask"></div>
 </div>
 
 <div class="hvr-grow">
-    <img src="../assets/images/covers/flow-caverna.png">
+    <a href="flow-caverna.php"><img src="../assets/images/covers/flow-caverna.png"></a>
     <span>Flow Caverna</span>
     <div class="mask"></div>
 </div>
 
 <div class="hvr-grow">
-    <img src="../assets/images/covers/mindfulness.png">
+    <a href="mindfulness.php"><img src="../assets/images/covers/mindfulness.png"></a>
     <span>Mindfulness</span>
     <div class="mask"></div>
 </div>
 
 <div class="hvr-grow">
-    <img src="../assets/images/covers/lembretes.png">
+    <a href="anotacoes.php"><img src="../assets/images/covers/lembretes.png"></a>
     <span>Anotações e Lembretes</span>
     <div class="mask"></div>
 </div>
 
 <div class="hvr-grow">
-    <img src="../assets/images/covers/conhecimento.png">
+    <a href="conhecimento.php"><img src="../assets/images/covers/conhecimento.png"></a>
     <span>Conhecimento</span>
     <div class="mask"></div>
 </div>
 
 <div class="hvr-grow">
-    <img src="../assets/images/covers/financeiro.png">
+    <a href="financeiro.php"><img src="../assets/images/covers/financeiro.png"></a>
     <span>Financeiro</span>
     <div class="mask"></div>
 </div>
 
 <div class="hvr-grow">
-    <img src="../assets/images/covers/conteudos.png">
+    <a href="conteudos.php"><img src="../assets/images/covers/conteudos.png"></a>
     <span>Conteúdos e Manual de Uso</span>
     <div class="mask"></div>
 </div>
